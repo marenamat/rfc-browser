14e2b031205dce80575076664be80e8cdce04309
Maria Matejka <mq@ucw.cz>
2025-08-16 01:05:57 +0200

    Run the updater in loop with random sleeping

--- update.py
+++ update.py
@@ -1,20 +1,30 @@
 import IETF
 import os
+import random
 import subprocess
+import time
 
-try:
-    loaded = IETF.refresh(limit=1)
+begin = True
+while True:
+    if begin:
+        begin = False
+    else:
+        time.sleep(random.randrange(5, 30))
 
-except IETF.DownloadException as e:
-    print(f"Failed to download from {e.request.url}: {e.request.status_code}")
-    print(e.request)
-    exit(1)
+    try:
+        print("Loading …")
+        loaded = IETF.refresh(limit=1)
 
-if len(loaded) == 0:
-    exit(0)
+    except IETF.DownloadException as e:
+        print(f"Failed to download from {e.request.url}: {e.request.status_code}")
+        print(e.request)
+        exit(1)
 
-assert(len(loaded) == 1)
+    if len(loaded) == 0:
+        exit(0)
 
-os.chdir(IETF.datadir)
-subprocess.run(["git", "add", "document/*", "submission/*", "meta.json"])
-subprocess.run(["git", "commit", "-m", f"Auto: {loaded[0].name}"])
+    assert(len(loaded) == 1)
+
+    os.chdir(IETF.datadir)
+    subprocess.run(["git", "add", "document/*", "submission/*", "meta.json"])
+    subprocess.run(["git", "commit", "-m", f"Auto: {loaded[0].name}"])
